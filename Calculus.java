0dd533f6c9c32cca556e8661977200515ca523ff
Karishma Patel <karishma1126@yahoo.com>
2013-09-08 16:22:07 -0600

    added code for e^x

--- Calculus.java
+++ Calculus.java
@@ -1,5 +1,30 @@
 public class Calculus {
 	public static void main(String[] args) {
 		
+		System.out.println("Enter a number: ");
+		Scanner reader = new Scanner (System.in);
+		int x = reader.nextInt();
+		int n = 1;
+		double sum = 1.0;
+
+		double lessThan = Math.pow(10,-12);
+		double term =7;
+		int factorial =1;
+		System.out.println("Limit value=" + lessThan);
+		while (term>=lessThan) {
+			factorial = 1;
+			for (int k=1; k<=n; k++) {
+				factorial = factorial *k;
+			}
+		}
+
+		double ex=Math.pow(x,n);
+		term = ex / factorial;
+		sum = sum + term;
+		System.out.println("n=" + n + "; Factorial= " + factorial + "; Term=" + term + "; Power= " + ex + "; Sum= " + sum);
+		n++;
+ 	
+
 	}
-}
+ } 
+     
